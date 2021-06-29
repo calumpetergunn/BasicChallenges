bf6017ff2217172ecee21edd07e6a89cb9c7363c
calumpetergunn <calumgunn@gmail.com>
2021-06-29 12:57:04 +0100

    added fizzbuzz with input

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 function fizzBuzz(){
-    for (let number = 0; number <= 100; number++){
+    for (let number = 1; number <= 100; number++){
         if (number % 15 === 0){
             console.log("FizzBuzz");
         }
@@ -15,5 +15,26 @@ function fizzBuzz(){
     }
 }
 
-
 fizzBuzz();
+
+function fizzBuzzWithInput(input){
+    if (input > 0 && input <= 100){
+        if (input % 15 === 0){
+            console.log("FizzBuzz");
+        }
+        else if (input % 5 === 0){
+            console.log("Buzz");
+        } 
+        else if (input % 3 === 0){
+            console.log("Fizz");
+        } 
+        else {
+            console.log(input);
+        }
+    }
+}
+
+fizzBuzzWithInput(15);
+fizzBuzzWithInput(25);
+fizzBuzzWithInput(9);
+fizzBuzzWithInput(4);
